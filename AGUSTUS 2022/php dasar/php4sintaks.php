34665e0ffc3eb46d06345dec2875a3ef05e83729
Denta <dentailham06@gmail.com>
2022-08-22 15:56:19 +0700

    update

---
+++ AGUSTUS 2022/php dasar/php4sintaks.php
@@ -0,0 +1,69 @@
+<?php
+        // Pertemuan 4 - PHP Dasar
+        // Sintakas PHP
+
+        // Standart Output
+        // echo, Print
+        // print_r
+        // var_dump
+
+        // Penulisan sintaks PHP
+        // 1. PHP di dalam HTML
+        // 2. HTML di dalam PHP
+
+        // Variabel dan Tipe Data
+        // Variabel
+        
+        $nama = "Denta";
+        echo "Halo, nama saya $nama";
+
+        // Operator
+        // aritmatika
+        // + - * / %
+        // $x = 10;
+        // $y = 20;
+        // echo $x * $y;
+        
+        // penggabung string / concatenation / concat
+        
+        $nama_depan = "Denta";
+        $nama_belakang = "Dhiwangga";
+        echo "<br>" . $nama_depan . $nama_belakang;
+
+        // Assignment
+        // =, +=, -=, *=, /=, %=, .=
+        // $x = 1;
+        // $x -= 5;
+        // echo $x;
+        $nama = "Rizqilla";
+        $nama .= " ";
+        $nama .="Ilham";
+        echo $nama;
+        
+        // perbandingan
+        // <, >, <=, >=, ==
+        // var_dump(1 == "1");
+
+        // identitas
+        // ===, !==
+        // var_dump (1 === "1")
+
+        // Logika
+        // &&, ||, !
+        // $x = 30;
+        // var_dump($x < 20 || $x % 2 == 0);
+  
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
