babea477c130cfcff163e650b61917677860e8da
Denta <dentailham06@gmail.com>
2022-08-23 15:47:25 +0700

    update

---
+++ AGUSTUS 2022/php dasar/php9database&mysql.php
@@ -0,0 +1,8 @@
+<!-- Pengertian database adalah sekumpulan data yang dikelola berdasarkan ketentuan
+tertentu yang saling berkaitan sehingga memudahkan dalam pengelolaannya. 
+Dihimpun dari berbagai sumber, secara sederhana, database atau basis data merupakan
+sekumpulan data atau informasi yang tersimpan secara sistematis.
+
+MySQL adalah sistem manajemen database relasional (RDBMS) open-source berbasis SQL 
+yang bekerja dengan model client-server. Kalau DBMS adalah sistem manajemen database
+secara umum, RDBMS merupakan software pengelolaan database berdasarkan model relasional -->
