91d69316573d6f044ac725da152748d8c26235ec
Denta <dentailham06@gmail.com>
2022-08-27 12:07:43 +0700

    update

--- AGUSTUS 2022/php dasar/sesi17/functions.php
+++ AGUSTUS 2022/php dasar/sesi17/functions.php
@@ -1,5 +1,8 @@
 <?php
 // koneksi ke database
+
+use function PHPSTORM_META\map;
+
 $conn = mysqli_connect("localhost", "root", "", "phpdasar");
 
 function query($query)
@@ -124,4 +127,41 @@ function hapus($id) {
     global $conn;
     mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
     return mysqli_affected_rows($conn);
+}
+
+
+function registrasi($data) {
+    global $conn;
+
+    $username = strtolower(stripslashes($data["username"] ) );
+    $password = mysqli_real_escape_string($conn, $data["password"] );
+    $password2 = mysqli_real_escape_string($conn, $data["password2"] );
+
+    // cek username anda sudah ada atau belum
+    $result = mysqli_query($conn, " SELECT username FROM user WHERE username = '$username'");
+
+    if( mysqli_fetch_assoc($result) ) {
+        echo "<script>
+                alert('username sudah terdaftar!')
+                </script>";
+        return false;        
+
+    }
+
+    // cek konfirmasi password
+    if($password !== $password2 ) {
+        echo "<script>
+                alert('konfirmasi password tidak sesuai!');
+              </script>";
+        return false;      
+    }
+
+    // enkripsi password
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    // tambahkan user baru ke database
+    mysqli_query($conn, "INSERT INTO user VALUES('', '$username', '$password')");
+
+    return mysqli_affected_rows($conn);
+
 }
